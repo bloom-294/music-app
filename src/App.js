a3c84b8975dc33f4f22b9554bcda1196ae5505d1
bloom-294 <non44921@gmail.com>
2024-08-16 19:03:55 +0900

    音楽再生・停止機能追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SongList } from "./components/SongList";
 import spotify from "./lib/spotify";
+import { Player } from "./components/Player";
 
 
 const App = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [popularSongs, setPopularSongs] = useState([]);
-  
+  const [isPlay, setIsPlay] = useState(false);
+  const [selectedSong, setSelectedSong] = useState();
+  const audioRef = useRef(null);
+
   useEffect(()=> {
     fetchPopularSongs();
   },[]);
@@ -22,6 +26,34 @@ const App = () => {
     setIsLoading(false);
   }
 
+  const handleSongSelected = async(song) => {
+    setSelectedSong(song);
+    if(song.preview_url != null) {
+      audioRef.current.src = song.preview_url;
+      playSong();
+    } else {
+      pauseSong();
+    }
+  }
+  
+  const playSong = () => {
+    audioRef.current.play();
+    setIsPlay(true);    
+  };
+
+  const pauseSong = () => {
+    audioRef.current.pause();
+    setIsPlay(false);    
+  };
+
+  const toggleSong = () => {
+    if(isPlay) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
@@ -30,9 +62,17 @@ const App = () => {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList isLoading={isLoading} songs={popularSongs} />
+          <SongList isLoading={isLoading} songs={popularSongs} onSongSelected={handleSongSelected} />
         </section>
       </main>
+      { selectedSong != null && (
+        <Player 
+          song={selectedSong} 
+          isPlay={isPlay} 
+          onButtonClick={toggleSong} 
+        /> 
+      )}
+      <audio ref={audioRef} />
     </div>
   );
 }
